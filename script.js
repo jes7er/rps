3d929bf025b173eb23fdc7f3150bdf26ed67d0bf
Tim Adams <adams.timothy.d@gmail.com>
2019-12-30 11:14:08 -0800

    Added loop for 5 rounds and modified js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function main() {
-//const humanScore = 0;
-//const compScore = 0;
 
 function computerPlay() {
     var options = ["rock","paper","scissors"];
@@ -8,34 +6,55 @@ function computerPlay() {
     return options[compSelect];
 }
 
+function humanPlay() {
+    var humanSelection = prompt("Enter 'rock', 'paper' or 'scissors'").toLowerCase();
+    return humanSelection;
+}
+
 function playRound(playerSelection, computerSelection) {
     //const para = document.querySelector('p');
     const para = document.getElementById('results');
     if (playerSelection === computerSelection) {
           para.innerHTML = '<p>Tie!</p>';
+          playRound(playerSelection, computerSelection);
       } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-          para.innerHTML = '<p>Human wins!</p>';
+          para.innerHTML = '<p>Human wins!<br>(computer chose scissors)</p>';
+          ++roundCount;
+          ++humanScore;
       } else if ((playerSelection === "rock") && (computerSelection === "paper")) {
-          para.innerHTML = '<p>Computer wins!</p>';
+          para.innerHTML = '<p>Computer wins!<br>(computer chose paper)</p>';
+          ++roundCount;
+          ++compScore;
       } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
-          para.innerHTML = '<p>Computer wins!</p>';
+          para.innerHTML = '<p>Computer wins!<br>(computer chose scissors)</p>';
+          ++compScore;
+          ++roundCount;
       } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
-          para.innerHTML = '<p>Player wins!</p>';
+          para.innerHTML = '<p>Player wins!<br>(computer chose rock)</p>';
+          ++humanScore;
+          ++roundCount;
       } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-          para.innerHTML = '<p>Player wins!</p>';
+          para.innerHTML = '<p>Player wins!<br>(computer chose paper)</p>';
+          ++humanScore;
+          ++roundCount;
       } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
-          para.innerHTML = '<p>Computer wins!</p>';
+          para.innerHTML = '<p>Computer wins!<br>(computer chose rock)</p>';
+          ++compScore;
+          ++roundCount;
       } else {
           para.innerHTML = '<p>You did not enter a valid choice!</p>';
+          playRound(playerSelection, computerSelection);
       }
 }
 
-let playerSelection = prompt("Enter rock, paper or scissors");
-const computerSelection = computerPlay();
+let playerSelection = humanPlay();
+let computerSelection = computerPlay();
 
+var compScore = 0, humanScore = 0;
 
-
-console.log(playRound(playerSelection, computerSelection));
+for (var roundCount = 0; ((compScore<5) && (humanScore<5); roundCount++ )) {
+    playRound(playerSelection, computerSelection);
+}
 
 }
 
