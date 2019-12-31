48d41c4c6a1c519c1ad9021fdafca31559cc74a2
Tim Adams <adams.timothy.d@gmail.com>
2019-12-30 20:46:16 -0800

    Moved appendChild to loop instead of playRound function

--- script.js
+++ script.js
@@ -18,36 +18,36 @@ function humanPlay() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
           content.textContent = 'Tie!';
-          container.appendChild(content);
+          //container.appendChild(content);
       } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
           content.textContent = 'You win! - computer chose scissors';
-          container.appendChild(content);
+          //container.appendChild(content);
           humanScore += 1;
       } else if ((playerSelection === "rock") && (computerSelection === "paper")) {
           content.textContent = 'Computer wins! - computer chose paper';
-          container.appendChild(content);
+          //container.appendChild(content);
           compScore += 1;
       } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
           content.textContent = 'Computer wins! - computer chose scissors';
-          container.appendChild(content);
+          //container.appendChild(content);
           compScore += 1;
       } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
           content.textContent = 'You win! - computer chose rock';
-          container.appendChild(content);
+          //container.appendChild(content);
           humanScore += 1;
       } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
           content.textContent = 'You win! - computer chose paper';
-          container.appendChild(content);
+          //container.appendChild(content);
           humanScore += 1;
       } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
           content.textContent = 'Computer wins! - computer chose rock';
-          container.appendChild(content);
+          //container.appendChild(content);
           compScore += 1;
       } else {
           content.textContent = 'You did not enter a valid choice!';
-          container.appendChild(content);
+          //container.appendChild(content);
       }
-    return humanScore,compScore;
+    return [content, humanScore, compScore];
 }
 
 var compScore = 0;
@@ -55,5 +55,6 @@ var humanScore = 0;
 
 
 while ((compScore < 5) && (humanScore < 5)) {
-    playRound(humanPlay(), computerPlay());
+    var result = playRound(humanPlay(), computerPlay());
+    container.appendChild(result[0] + 'Your score: '+result[1]+', Computer score: '+result[2]);
 } 
