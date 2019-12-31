ba305af7095ede4b5a2854f00e1308b54495bb03
Tim Adams <adams.timothy.d@gmail.com>
2019-12-30 18:42:41 -0800

    Corrected div id reference name

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('#main');
+const container = document.querySelector('#container');
 const content = document.createElement('div');
 
 content.classList.add('results');
